1279231f822666287ac7be3bcf8be09de3105353
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-11-15 23:48:38 -0500

    urls added

---
+++ front-end/src/utilities/index.js
@@ -0,0 +1,3 @@
+const textBox = 'https://eddie-first-test-bucket.s3.us-east-2.amazonaws.com/textBox.png';
+const banner = 'https://eddie-first-test-bucket.s3.us-east-2.amazonaws.com/banner.png';
+const background = 'https://eddie-first-test-bucket.s3.us-east-2.amazonaws.com/background.jpeg';
