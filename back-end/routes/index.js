c7a9ed35ff76bf43de3b7ce67534e37036c0bae4
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-11-19 23:56:33 -0500

    database connected

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql');
+
+const config = require('../config/config');
+
+const { db } = config;
+const connection = mysql.createConnection(db);
+connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/test', (req, res) => {
+  const selectionQuery = `SELECT * from test;`;
+  connection.query(selectionQuery, [], (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 module.exports = router;
